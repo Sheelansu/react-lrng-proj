fae6627b54dcdb72c3000fe83a45e9d4f30e9601
Sheelansu Ujjain <sheelansu.ujjain@hotmail.com>
2024-03-10 23:05:49 +0530

    bottom-bar-fixed

--- rct-bgn/src/App.jsx
+++ rct-bgn/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import Cards from './Cards'
+import BgChanger from './BgChanger'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("lavender")
 
   const emp1 = {
     uName : 'Sher',
@@ -16,11 +17,49 @@ function App() {
     uImg : "https://www.simplilearn.com/ice9/free_resources_article_thumb/What-Skills-Do-I-Need-to-Become-a-Data-Scientist.jpg"
   }
   return (
-    <>
-      <Cards someObj = {emp1} />
-      <Cards someObj = {emp2} />
-      <Cards />
-    </>
+      <div className='w-full h-full duration-200 flex justify-center' style={{backgroundColor : color}}>
+        <div>
+        <Cards someObj = {emp1} />
+        <Cards someObj = {emp2} />
+        <Cards />
+        </div>
+        <div className='fixed bottom-3 flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={()=>{setColor('violet')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+           style={{backgroundColor : 'violet'}}
+          >VIOLET</button>
+
+          <button onClick={()=>{setColor('indigo')}}
+          className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'indigo'}}
+          >INDIGO</button>
+
+          <button onClick={()=>{setColor('blue')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'blue'}}
+          >BLUE</button>
+
+          <button onClick={()=>{setColor('green')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'green'}}
+          >GREEN</button>
+
+          <button onClick={()=>{setColor('yellow')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'yellow'}}
+          >YELLOW</button>
+
+          <button onClick={()=>{setColor('orange')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'orange'}}
+          >ORANGE</button>
+
+          <button onClick={()=>{setColor('red')}}
+           className=' outline-none px-4 py-1 rounded-full text-white shadow-lg'
+          style={{backgroundColor : 'red'}}
+          >RED</button>
+        </div>
+      </div>
   )
 }
 
